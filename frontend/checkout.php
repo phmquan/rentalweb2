a0e64727b394a8ffdb921965972bea10b5b782d8
Triết Nguyễn Phạm <triet.pnc@gmail.com>
2023-12-26 21:17:20 +0700

    update frontend

--- frontend/checkout.php
+++ frontend/checkout.php
@@ -55,33 +55,34 @@
     </div> <!-- End header area -->
 
    <div class="site-branding-area">
-        <div class="container">
-            <div class="row">			
-				<div class="col-md-7">	
-				<div class="shopping-item">
-				<div class="navbar-header">
-				<h1><a href="webpage.php"><img src="img/brand3.png"></a></h1>
-				</div>
-				<div class="navbar-header">
-				<a class="navbar-brand" href="#"></a>				
-				</div>
-                    <ul class="nav navbar-nav">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <div class="shopping-item">
+                    <div class="navbar-header">
+                        <h1><a href="webpage.php"><img style="width: 173px; height: 50px;" src="img/brand3.png"></a></h1>
+                    </div>
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="#"></a>
+                    </div>
+                    <ul class="nav navbar-nav" >
+                        
                         <li class="active"><a href="webpage.php">Home</a></li>
-                        <li><a href="shop.php">All Product</a></li>
+                        <li><a href="ListOfProducts.php">List Of Products</a></li>
                     </ul>
-                
-				</div>
-				</div>
-                <div class="col-sm-5">
-                    <div class="shopping-item">
-					<form action="#">
-									<input type="text" placeholder="Search products...">
-									<input type="submit" value="Search">
-					</form>                    
-				</div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="shopping-item">
+                    <form action="#">
+                        <input type="text" placeholder="Search products...">
+                        <input type="submit" value="Search">
+                    </form>
                 </div>
             </div>
         </div>
+    </div>
+
     </div>
     
     <div class="product-big-title-area">
@@ -101,51 +102,8 @@
         <div class="zigzag-bottom"></div>
         <div class="container">
             <div class="row">
-                <div class="col-md-4">                
-                    <div class="single-sidebar">
-                        <h2 class="sidebar-title">Products</h2>
-                        <div class="thubmnail-recent">
-                            <img src="img/product-7.jpg" class="recent-thumb" alt="">
-                            <h2>The Legend of Tarzan</h2>
-                                <div class="product-carousel-price">
-                                    <ins>Rp.289.000</ins> <del>Rp.499.000</del>
-                                </div>                             
-                        </div>
-                        <div class="thubmnail-recent">
-                            <img src="img/product-8.jpg" class="recent-thumb" alt="">
-                            <h2>Shutter Island</h2>
-                                <div class="product-carousel-price">
-                                    <ins>Rp.94.000</ins> <del>Rp.125.000</del>
-                                </div>                             
-                        </div>
-                        <div class="thubmnail-recent">
-                            <img src="img/product-9.jpg" class="recent-thumb" alt="">
-                            <h2><a href="single-product.php">The Raid Redemption</a></h2>
-                                <div class="product-carousel-price">
-                                    <ins>Rp.120.000</ins> <del>Rp.225.000</del>
-                                </div>                             
-                        </div>
-                        <div class="thubmnail-recent">
-                            <img src="img/product-10.jpg" class="recent-thumb" alt="">
-                            <h2>The Fault in Our Stars</h2>
-                                <div class="product-carousel-price">
-                                    <ins>$220.000</ins> <del>RP.355.000</del>
-                                </div>                            
-                        </div>
-                    </div>
-                    
-                    <div class="single-sidebar">
-                        <h2 class="sidebar-title">Recent Posts</h2>
-                        <ul>
-                            <li><a href="">Big Mommas</a></li>
-                            <li><a href="">The Angry Birds Movie</a></li>
-                            <li><a href="">The Mechanic</a></li>
-                            <li><a href="">Alvin and The Chipmunks</a></li>
-                            <li><a href="">The Eagle</a></li>
-                        </ul>
-                    </div>
-                </div>
-                
+                <!--SIDEBAR HERE-->
+                 <?php require('includes/sidebar_webpage.php'); ?>
                 <div class="col-md-8">
                     <div class="product-content-right">
                         <div class="woocommerce">
@@ -557,79 +515,9 @@
     </div>
 
 
-<div class="footer-top-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3 col-sm-6">
-                    <div class="footer-about-us">
-                        <h2><a href="webpage.php">DVDTrendy</a></h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis sunt id doloribus vero quam laborum quas alias dolores blanditiis iusto consequatur, modi aliquid eveniet eligendi iure eaque ipsam iste, pariatur omnis sint! Suscipit, debitis, quisquam. Laborum commodi veritatis magni at?</p>
-                        <div class="footer-social">
-                            <a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
-                            <a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
-                            <a href="#" target="_blank"><i class="fa fa-youtube"></i></a>
-                            <a href="#" target="_blank"><i class="fa fa-linkedin"></i></a>
-                        </div>
-                    </div>
-                </div>
-                
-                <div class="col-md-3 col-sm-6">
-                    <div class="footer-menu">
-                        <h2 class="footer-wid-title">User Navigation </h2>
-                        <ul>
-                            <li><a href="profile.php">My account</a></li>
-                            <li><a href="shop.php">Shop</a></li>
-                            <li><a href="cart.php">Cart</a></li>
-                            <li><a href="checkout.php">Check Out</a></li>
-                        </ul>                        
-                    </div>
-                </div>
-                
-                <div class="col-md-3 col-sm-6">
-                    <div class="footer-menu">
-                        <h2 class="footer-wid-title">Categories</h2>
-                        <ul>
-                            <li><a href="webpage.php">Home</a></li>
-                            <li><a href="shop.php">New Realese</a></li>
-                            <li><a href="shop.php">Top Rated Film</a></li>
-                            <li><a href="shop.php">Search</a></li>
-                        </ul>                        
-                    </div>
-                </div>
-                
-                <div class="col-md-3 col-sm-6">
-                    <div class="footer-newsletter">
-                        <h2 class="footer-wid-title">Newsletter</h2>
-                        <p>Sign up to our newsletter and get exclusive deals you wont find anywhere else straight to your inbox!</p>
-                        <div class="newsletter-form">
-                            <form action="#">
-                                <input type="email" placeholder="Type your email">
-                                <input type="submit" value="Subscribe">
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> <!-- End footer top area -->
+    <?php require('includes/footer_webpage.php'); ?>
+
     
-    <div class="footer-bottom-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4">
-				<br/><br/>
-                2023 © DVDTrendy    
-                </div>
-                
-                <div class="col-md-8">
-                    <div class="footer-card-icon">
-						<img src="img/payment.png" alt="">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> <!-- End footer bottom area -->
-   
     <!-- Latest jQuery form server -->
     <script src="https://code.jquery.com/jquery.min.js"></script>
     
--- frontend/webpage.php
+++ frontend/webpage.php
@@ -1,22 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>DVDTren</title>
-    
+    <title>DVDTrendy</title>
+
     <!-- Google Fonts -->
-    <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600' rel='stylesheet'
+        type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
-    
+
     <!-- Bootstrap -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    
+
     <!-- Font Awesome -->
     <link rel="stylesheet" href="css/font-awesome.min.css">
-    
+
     <!-- Custom CSS -->
     <link rel="stylesheet" href="css/owl.carousel.css">
     <link rel="stylesheet" href="style.css">
@@ -28,9 +30,9 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-  </head>
+</head>
 
-  <body>
+<body>
     <div class="header-area">
         <div class="container">
             <div class="row">
@@ -39,7 +41,8 @@
                 <div class="col-md-5">
                     <div class="header-right">
                         <ul class="list-unstyled list-inline">
-							<li><a href="cart.php"><i class="glyphicon glyphicon-shopping-cart"></i> Cart - <span class="cart-amunt">$0</span>  </a></li>
+                            <li><a href="cart.php"><i class="glyphicon glyphicon-shopping-cart"></i> Cart - <span
+                                        class="cart-amunt">$0</span> </a></li>
                             <?php
                             include "./model/user.php";
                             session_start();
@@ -62,36 +65,36 @@
         </div>
     </div> <!-- End header area -->
 
-   <div class="site-branding-area">
+    <div class="site-branding-area">
         <div class="container">
-            <div class="row">			
-				<div class="col-md-7">	
-				<div class="shopping-item">
-				<div class="navbar-header">
-				<h1><a href="webpage.php">DVDTrendy</a></h1>
-				</div>
-				<div class="navbar-header">
-				<a class="navbar-brand" href="#"></a>				
-				</div>
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="webpage.php">Home</a></li>
-                        <li><a href="ListOfProducts.php">List of Products</a></li>
-                    </ul>
-                
-				</div>
-				</div>
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="shopping-item">
+                        <div class="navbar-header">
+                            <h1><a href="webpage.php">DVDTrendy</a></h1>
+                        </div>
+                        <div class="navbar-header">
+                            <a class="navbar-brand" href="#"></a>
+                        </div>
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="webpage.php">Home</a></li>
+                            <li><a href="ListOfProducts.php">List of Products</a></li>
+                        </ul>
+
+                    </div>
+                </div>
                 <div class="col-sm-5">
                     <div class="shopping-item">
-					<form action="search.php" method = "post" target = "_blank">
-									<input type="text" name="search_query" placeholder="Search products...">
-									<input type="submit" value="Search">
-					</form>                    
-				</div>
+                        <form action="search.php" method="post" target="_blank">
+                            <input type="text" name="search_query" placeholder="Search products...">
+                            <input type="submit" value="Search">
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    
+
     <?php
 $conn = connectdb();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -102,33 +105,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     exit();
 }
 ?>
-   
-<div class="slider-area">
-        	<!-- Slider -->
-			<div class="block-slider block-slider4">
-				<ul class="" id="bxslider-home4">
-					<li>
-						<img src="img/h4-slide.png" alt="Slide">
-					</li>
-					<li><img src="img/h4-slide2.png" alt="Slide">
-					</li>
-					<li><img src="img/h4-slide3.png" alt="Slide">
-					</li>
-					<li><img src="img/h4-slide4.png" alt="Slide">
-					</li>
-				</ul>
-			</div>
-			<!-- ./Slider -->
+
+    <div class="slider-area">
+        <!-- Slider -->
+        <div class="block-slider block-slider4">
+            <ul class="" id="bxslider-home4">
+                <li>
+                    <img src="img/h4-slide.png" alt="Slide">
+                </li>
+                <li><img src="img/h4-slide2.png" alt="Slide">
+                </li>
+                <li><img src="img/h4-slide3.png" alt="Slide">
+                </li>
+                <li><img src="img/h4-slide4.png" alt="Slide">
+                </li>
+            </ul>
+        </div>
+        <!-- ./Slider -->
     </div> <!-- End slider area -->
-   
+
     <div class="mainmenu-area">
         <div class="container">
             <div class="row">
-                				
+
             </div>
         </div>
     </div> <!-- End mainmenu area -->
-	
+
     <div class="promo-area">
         <div class="zigzag-bottom"></div>
         <div class="container">
@@ -160,141 +163,143 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </div>
         </div>
     </div> <!-- End promo area -->
-    
+
     <div class="maincontent-area">
         <div class="zigzag-bottom"></div>
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <div >
-						<div class="product-big-title-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="product-bit-title text-center">
-                        <h2>Pick Your CD</h2>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> <!-- End Page title area -->
-    <div id="product-container"></div>
-	<script>
-        let start = 16;
-let xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        // Assuming you have a container with the id "product-container" to display the products
-        document.getElementById("product-container").innerHTML = this.responseText;
-    }
-};
-xhttp.open("GET", "ShowListProduct.php?start=" + start, true);
-xhttp.send();
-
-    </script>
-					<br><br>
-					<ul class="pager">
-  <!-- <li><a href="#">Previous</a></li>
+                    <div>
+                        <div class="product-big-title-area">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="product-bit-title text-center">
+                                            <h2>Pick Your CD</h2>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div> <!-- End Page title area -->
+                        <div id="product-container"></div>
+                        <script>
+                        let start = 16;
+                        let xhttp = new XMLHttpRequest();
+                        xhttp.onreadystatechange = function() {
+                            if (this.readyState == 4 && this.status == 200) {
+                                // Assuming you have a container with the id "product-container" to display the products
+                                document.getElementById("product-container").innerHTML = this.responseText;
+                            }
+                        };
+                        xhttp.open("GET", "ShowListProduct.php?start=" + start, true);
+                        xhttp.send();
+                        </script>
+                        <br><br>
+                        <ul class="pager">
+                            <!-- <li><a href="#">Previous</a></li>
   <li><a href="#">Next</a></li> -->
-</ul>
-                </div>
-            </div>
-        </div>
-    </div> <!-- End main content area -->
-    
-    
-    <div class="footer-top-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3 col-sm-6">
-                    <div class="footer-about-us">
-                        <h2><a href="webpage.php">DVDTrendy</a></h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis sunt id doloribus vero quam laborum quas alias dolores blanditiis iusto consequatur, modi aliquid eveniet eligendi iure eaque ipsam iste, pariatur omnis sint! Suscipit, debitis, quisquam. Laborum commodi veritatis magni at?</p>
-                        <div class="footer-social">
-                            <a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
-                            <a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
-                            <a href="#" target="_blank"><i class="fa fa-youtube"></i></a>
-                            <a href="#" target="_blank"><i class="fa fa-linkedin"></i></a>
-                        </div>
-                    </div>
-                </div>
-                
-                <div class="col-md-3 col-sm-6">
-                    <div class="footer-menu">
-                        <h2 class="footer-wid-title">User Navigation </h2>
-                        <ul>
-                            <li><a href="profile.php">My account</a></li>
-                            <li><a href="shop.php">Shop</a></li>
-                            <li><a href="cart.php">Cart</a></li>
-                            <li><a href="checkout.php">Check Out</a></li>
-                        </ul>                        
-                    </div>
-                </div>
-                
-                <div class="col-md-3 col-sm-6">
-                    <div class="footer-menu">
-                        <h2 class="footer-wid-title">Categories</h2>
-                        <ul>
-                            <li><a href="index.php">Home</a></li>
-                            <li><a href="shop.php">New Realese</a></li>
-                            <li><a href="shop.php">Top Rated Film</a></li>
-                            <li><a href="shop.php">Search</a></li>
-                        </ul>                        
-                    </div>
-                </div>
-                
-                <div class="col-md-3 col-sm-6">
-                    <div class="footer-newsletter">
-                        <h2 class="footer-wid-title">Newsletter</h2>
-                        <p>Sign up to our newsletter and get exclusive deals you wont find anywhere else straight to your inbox!</p>
-                        <div class="newsletter-form">
-                            <form action="#">
-                                <input type="email" placeholder="Type your email">
-                                <input type="submit" value="Subscribe">
-                            </form>
-                        </div>
+                        </ul>
                     </div>
                 </div>
             </div>
-        </div>
-    </div> <!-- End footer top area -->
-    
-    <div class="footer-bottom-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4">
-				<br/><br/>
-                2023 © DVDTrendy 
-                </div>
-                
-                <div class="col-md-8">
-                    <div class="footer-card-icon">
-						<img src="img/payment.png" alt="">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> <!-- End footer bottom area -->
+        </div> <!-- End main content area -->
 
-   
-    <!-- Latest jQuery form server -->
-    <script src="https://code.jquery.com/jquery.min.js"></script>
-    
-    <!-- Bootstrap JS form CDN -->
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-    
-    <!-- jQuery sticky menu -->
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/jquery.sticky.js"></script>
-    
-    <!-- jQuery easing -->
-    <script src="js/jquery.easing.1.3.min.js"></script>
-    
-    <!-- Main Script -->
-    <script src="js/main.js"></script>
-    
-    <!-- Slider -->
-    <script type="text/javascript" src="js/bxslider.min.js"></script>
-	<script type="text/javascript" src="js/script.slider.js"></script>
-  </body>
+
+        <div class="footer-top-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3 col-sm-6">
+                        <div class="footer-about-us">
+                            <h2><a href="webpage.php">DVDTrendy</a></h2>
+                            <p>
+                                Chào mừng bạn đến với DVDTRENDY - Nơi Cho Thuê DVD Trực Tuyến! Thư viện phim đa dạng với hơn 1000 bộ phim từ mọi thể loại. Giao hàng nhanh chóng trong vòng 24h. Ưu đãi khủng 30% cho đơn hàng đầu tiên. Đặt ngay để trải nghiệm giải trí sắc nét!</p>
+                            <div class="footer-social">
+                                <a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
+                                <a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
+                                <a href="#" target="_blank"><i class="fa fa-youtube"></i></a>
+                                <a href="#" target="_blank"><i class="fa fa-linkedin"></i></a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 col-sm-6">
+                        <div class="footer-menu">
+                            <h2 class="footer-wid-title">User Navigation </h2>
+                            <ul>
+                                <li><a href="profile.php">My account</a></li>
+                                <li><a href="shop.php">Shop</a></li>
+                                <li><a href="cart.php">Cart</a></li>
+                                <li><a href="checkout.php">Check Out</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 col-sm-6">
+                        <div class="footer-menu">
+                            <h2 class="footer-wid-title">Categories</h2>
+                            <ul>
+                                <li><a href="index.php">Home</a></li>
+                                <li><a href="shop.php">New Realese</a></li>
+                                <li><a href="shop.php">Top Rated Film</a></li>
+                                <li><a href="shop.php">Search</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3 col-sm-6">
+                        <div class="footer-newsletter">
+                            <h2 class="footer-wid-title">Newsletter</h2>
+                            <p>Sign up to our newsletter and get exclusive deals you wont find anywhere else straight to
+                                your inbox!</p>
+                            <div class="newsletter-form">
+                                <form action="#">
+                                    <input type="email" placeholder="Type your email">
+                                    <input type="submit" value="Subscribe">
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- End footer top area -->
+
+        <div class="footer-bottom-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <br /><br />
+                        2023 © DVDTrendy
+                    </div>
+
+                    <div class="col-md-8">
+                        <div class="footer-card-icon">
+                            <img src="img/payment.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- End footer bottom area -->
+
+
+        <!-- Latest jQuery form server -->
+        <script src="https://code.jquery.com/jquery.min.js"></script>
+
+        <!-- Bootstrap JS form CDN -->
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+        <!-- jQuery sticky menu -->
+        <script src="js/owl.carousel.min.js"></script>
+        <script src="js/jquery.sticky.js"></script>
+
+        <!-- jQuery easing -->
+        <script src="js/jquery.easing.1.3.min.js"></script>
+
+        <!-- Main Script -->
+        <script src="js/main.js"></script>
+
+        <!-- Slider -->
+        <script type="text/javascript" src="js/bxslider.min.js"></script>
+        <script type="text/javascript" src="js/script.slider.js"></script>
+</body>
+
 </html>
